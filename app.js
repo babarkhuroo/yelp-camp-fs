a342e39753260a9e5dbeb6c0a00d3250000a6227
babar <systemrockstar96@gmail.com>
2022-08-16 12:32:54 +0530

    campground index

--- app.js
+++ app.js
@@ -19,13 +19,9 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-  const camp = new Campground({
-    title: 'My Backyard',
-    description: 'Cheap Camping',
-  })
-  await camp.save()
-  res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({})
+  res.render('campgrounds', { campgrounds })
 })
 
 app.listen('3000', (req, res) => {
